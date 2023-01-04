815595ce0a5ecb3efaf8cef79d4910f49dac359b
AnthonyK213 <tjjzqwh@163.com>
2023-01-04 23:24:33 +0800

    clean

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,10 +5,6 @@
 
 mod v2ray_handler;
 
-use std::{
-    fs::{self, OpenOptions},
-    io::{self, Read, Write},
-};
 use tauri::{Manager, RunEvent, State, Window};
 use v2ray_handler::V2rayHandler;
 
@@ -28,39 +24,12 @@ fn v2ray_disconnect(state: State<'_, V2rayHandler>, window: Window) {
     state.stop_and_emit(&window);
 }
 
-#[tauri::command]
-fn read_file(path: String) -> Result<String, String> {
-    let mut buf = String::new();
-    let mapper = |e: io::Error| e.to_string();
-    let mut file = fs::File::open(path).map_err(mapper)?;
-    let _ = file.read_to_string(&mut buf).map_err(mapper)?;
-    Ok(buf)
-}
-
-#[tauri::command]
-fn write_file(path: String, content: String) -> Result<(), String> {
-    let mapper = |e: io::Error| e.to_string();
-    let mut file = OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .create_new(!fs::metadata(&path).is_ok())
-        .open(path)
-        .map_err(mapper)?;
-    file.write_all(content.as_bytes()).map_err(mapper)?;
-    Ok(())
-}
-
 fn main() {
     let v_handler = V2rayHandler::new();
 
     tauri::Builder::default()
         .manage(v_handler)
-        .invoke_handler(tauri::generate_handler![
-            v2ray_connect,
-            v2ray_disconnect,
-            read_file,
-            write_file
-        ])
+        .invoke_handler(tauri::generate_handler![v2ray_connect, v2ray_disconnect])
         .build(tauri::generate_context!())
         .expect("error while building tauri application")
         .run(|app_handle, event| match event {
